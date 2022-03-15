792547a2433b61d5696b9a8cfb2076742ad5690c
Rajat <rajatagarwal1709@gmail.com>
2022-03-15 13:15:06 +0530

    First

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar()
         <nav>
          <img src="./images/reactjs-icon.png" alt="image" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
+            <h4 className="nav--title">React Course - Projects</h4>
         </nav>
     )
 }
