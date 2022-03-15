276086fc39715c8c80942c493c78220bc8b383cb
Rajat <rajatagarwal1709@gmail.com>
2022-03-15 13:12:03 +0530

    https://github.com/rajat-1709/ReactPage.git

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function Navbar()
+{
+    return(
+        <nav>
+         <img src="./images/reactjs-icon.png" alt="image" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
